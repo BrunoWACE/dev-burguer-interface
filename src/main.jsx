ec2da92747a2cbc3942976b31a31fbea95152565
Bruno Alves Ferreira <brunowace.saikou@gmail.com>
2026-02-21 00:19:11 -0300

    add. eslintrc and prettierrc all json

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Login } from './containers/login'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { Login } from './containers/login';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Login />
   </StrictMode>,
-)
+);
