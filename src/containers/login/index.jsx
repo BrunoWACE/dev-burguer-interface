1669ace110ee944a1243953e04a27f98f8018a38
Bruno Alves Ferreira <brunowace.saikou@gmail.com>
2026-02-28 20:35:26 -0300

    Criando a tela de Usuário

--- src/containers/login/index.jsx
+++ src/containers/login/index.jsx
@@ -1,10 +1,12 @@
 import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
 
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 
 import Logo from '../../assets/logo.svg';
 import { Button } from '../../components/Button';
+import { api } from '../../services/api';
 import {
   Container,
   Form,
@@ -17,8 +19,14 @@ import {
 export function Login() {
   const scherma = yup
     .object({
-      email: yup.string().email().required(),
-      password: yup.string().min(6).required(),
+      email: yup
+        .string()
+        .email('Digite um e-mail válido')
+        .required('O email é obrigatório'),
+      password: yup
+        .string()
+        .min(6, 'A senha deve ter pelo menos 6 caracteres')
+        .required('Digite uma senha'),
     })
     .required();
 
@@ -30,7 +38,23 @@ export function Login() {
     resolver: yupResolver(scherma),
   });
 
-  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
+  const onSubmit = async (data) => {
+    const response = await toast.promise(
+      api.post('/sessions', {
+        email: data.email,
+        password: data.password,
+      }),
+      {
+        pending: 'Verificando seus dados...',
+        success: 'Seja Bem-vindo(a)!',
+        error: 'Email ou senha incorretas!',
+      },
+    );
+
+    console.log(response);
+  };
 
   return (
     <Container>
@@ -52,11 +76,13 @@ export function Login() {
           <InputContainer>
             <label>Email</label>
             <input type="email" {...register('email')} />
+            <p>{errors?.email?.message}</p>
           </InputContainer>
 
           <InputContainer>
             <label>Senha</label>
             <input type="password" {...register('password')} />
+            <p>{errors?.password?.message}</p>
           </InputContainer>
           <Button type="submit">Entrar</Button>
         </Form>
