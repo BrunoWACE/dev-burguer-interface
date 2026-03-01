a73f454c075d8bd777589cd5abac22f240c8fbea
Bruno Alves Ferreira <brunowace.saikou@gmail.com>
2026-03-01 12:46:18 -0300

    Tratando erros

---
+++ src/containers/Register/index.jsx
@@ -0,0 +1,116 @@
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+import Logo from '../../assets/logo.svg';
+import { Button } from '../../components/Button';
+import { api } from '../../services/api';
+import {
+  Container,
+  Form,
+  InputContainer,
+  LeftContainer,
+  RightContainer,
+  Title,
+} from './styles';
+
+export function Register() {
+  const scherma = yup
+    .object({
+      name: yup.string().required('O nome é obrigatório'),
+      email: yup
+        .string()
+        .email('Digite um e-mail válido')
+        .required('O email é obrigatório'),
+      password: yup
+        .string()
+        .min(6, 'A senha deve ter pelo menos 6 caracteres')
+        .required('Digite uma senha'),
+      confirmPassword: yup
+        .string()
+        .oneOf([yup.ref('password')], 'As senhas devem ser iguais')
+        .required('Confirme sua senha'),
+    })
+    .required();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(scherma),
+  });
+
+  console.log(errors);
+
+  const onSubmit = async (data) => {
+    try {
+      const { status } = await api.post(
+        '/users',
+        {
+          name: data.name,
+          email: data.email,
+          password: data.password,
+        },
+        {
+          validateStatus: () => true,
+        },
+      );
+
+      if (status === 200 || status === 201) {
+        toast.success('Conta criada com sucesso!');
+      } else if (status === 400) {
+        toast.error('Email já cadastrado! Faça o login para continuar.');
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      toast.error('😭 Falha no Sistema! Tente novamente');
+    }
+
+    console.log(status);
+  };
+
+  return (
+    <Container>
+      <LeftContainer>
+        <img src={Logo} alt="logo-devburguer" />
+      </LeftContainer>
+      <RightContainer>
+        <Title>Criar Conta</Title>
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <InputContainer>
+            <label>Name</label>
+            <input type="text" {...register('name')} />
+            <p>{errors?.name?.message}</p>
+          </InputContainer>
+
+          <InputContainer>
+            <label>Email</label>
+            <input type="email" {...register('email')} />
+            <p>{errors?.email?.message}</p>
+          </InputContainer>
+
+          <InputContainer>
+            <label>Senha</label>
+            <input type="password" {...register('password')} />
+            <p>{errors?.password?.message}</p>
+          </InputContainer>
+
+          <InputContainer>
+            <label>Confirmar Senha</label>
+            <input type="password" {...register('confirmPassword')} />
+            <p>{errors?.confirmPassword?.message}</p>
+          </InputContainer>
+
+          <Button type="submit">Criar Conta</Button>
+        </Form>
+        <p>
+          Já possui conta? <a>Clique aqui.</a>
+        </p>
+      </RightContainer>
+    </Container>
+  );
+}
